a889f009f31ae5a47653993346b3730a84d59d8f
mondira06 <mondira.chanda@anotechsolutions.com>
2025-12-12 16:55:19 +0530

    home, bottom

--- src/components/common/BottomNavigation.jsx
+++ src/components/common/BottomNavigation.jsx
@@ -31,7 +31,7 @@ const BottomNavigationArea = () => {
         style={{
           position: "fixed",
           bottom: "0px",
-          height: "10%",
+          height: "8.5%",
           // backgroundImage: "url(/assets/christmas2-CQdl-tRr.svg)",
           // backgroundSize: "cover",
           // backgroundPosition: "top",
@@ -44,40 +44,42 @@ const BottomNavigationArea = () => {
       >
         <img src="/assets/christmas2-CQdl-tRr.webp" alt="" width={"100%"} style={{ position: "absolute", top: "-38px", zIndex:10, marginBottom: "20px"}} />
         <BottomNavigationAction
-          style={{ color: value === "/promotion" ? "#ffffff" : "#ffffff" }}
+          style={{ color: value === "/promotion" ? "#24ee89" : "#ffffff", fontWeight: 600 }}
           label="Promotion"
           value="/promotion"
           icon={
             <img
               src={
                 value === "/promotion"
-                  ? "/assets/navigation/promotion.svg"
+                  ? "/assets/navigation/promotion1.svg"
                   : "/assets/navigation/promotion.svg"
               }
               width="30px"
               height="30px"
               style={{
-                marginBottom: "4px",
+               marginBottom: "2px",
+                paddingTop: "12px"
               }}
               alt="icon"
             />
           }
         />
         <BottomNavigationAction
-          style={{ color: value === "/activity" ? "#ffffff" : "#ffffff" }}
+          style={{ color: value === "/activity" ? "#24ee89" : "#ffffff", fontWeight: 600 }}
           label="Activity"
           value="/activity"
           icon={
             <img
               src={
                 value === "/activity"
-                  ? "/assets/navigation/activity.svg"
+                  ? "/assets/navigation/activity1.svg"
                   : "/assets/navigation/activity.svg"
               }
               width="30px"
               height="30px"
               style={{
-                marginBottom: "4px",
+                marginBottom: "2px",
+                paddingTop: "12px",
               }}
               alt="icon"
             />
@@ -89,52 +91,54 @@ const BottomNavigationArea = () => {
           value="/"
           icon={
             <img
-              src="/assets/navigation/home.webp"
-              width="50px"
-              height="50px"
+              src="/assets/navigation/home.png"
+              width="92px"
+              height="62px"
               alt="icon"
               style={{
                 zIndex: 20
               }}
             />
           }
-          style={{ color: value === "/" ? "#ffffff" : "#ffffff", marginBottom: "20px" }}
+          style={{ color: value === "/" ? "#ffffff" : "#ffffff", marginBottom: "0px" }}
         />
         <BottomNavigationAction
-          style={{ color: value === "/wallet" ? "#ffffff" : "#ffffff" }}
+          style={{ color: value === "/wallet" ? "#24ee89" : "#ffffff", fontWeight: 600 }}
           label="Wallet"
           value="/wallet"
           icon={
             <img
               src={
                 value === "/wallet"
-                  ? "/assets/navigation/wallet.svg"
+                  ? "/assets/navigation/wallet1.svg"
                   : "/assets/navigation/wallet.svg"
               }
               width="30px"
               height="30px"
               style={{
-                marginBottom: "4px", // hide image when src is empty
+                marginBottom: "2px",
+                paddingTop: "12px", // hide image when src is empty
               }}
               alt="icon"
             />
           }
         />
         <BottomNavigationAction
-          style={{ color: value === "/account" ? "#ffffff" : "#ffffff" }}
+          style={{ color: value === "/account" ? "#24ee89" : "#ffffff", fontWeight: 600 }}
           label="Account"
           value="/account"
           icon={
             <img
               src={
                 value === "/account"
-                  ? "/assets/navigation/account.svg"
+                  ? "/assets/navigation/account1.svg"
                   : "/assets/navigation/account.svg"
               }
               width="30px"
               height="30px"
               style={{
-                marginBottom: "4px",
+                marginBottom: "2px",
+                paddingTop: "12px",
               }}
               alt="icon"
             />
